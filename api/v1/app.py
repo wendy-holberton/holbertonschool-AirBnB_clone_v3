f863f35af1c3a402aa7c66a7997e43b405a524d8
Jenny <5704@holbertonstudents.com>
2023-04-11 20:07:07 -0700

    Added app variable which is an instance of Flask

---
+++ api/v1/app.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+from models import storage
+from api.v1.views import app_views
+from flask import Blueprint
+from flask import Flask
+import os
+import threading
+
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def close_storage(self):
+    storage.close()
+
+
+if __name__ == "__main__":
+    host_ip = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    host_port = os.getenv('HBNB_API_PORT', '5000')
+    app.run(host=host_ip, port=host_port, threaded=True)
