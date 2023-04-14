7fff35d39cd7a0fbfcc45f09101149e80b5f37d7
rrarima <5708@holbertonstudents.com>
2023-04-14 12:30:41 +1000

    implemented http request to new domain

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,13 +7,14 @@ from api.v1.views import app_views
 from flask import Blueprint
 from flask import Flask
 from flask import jsonify
+from flask_cors import CORS
 import os
 import threading
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
 
 @app.teardown_appcontext
 def close_storage(self):
