3b6acd4c1069bc36d197beb0d6a044c49f6966ed
Eps (Rarima) <113666607+rrarima@users.noreply.github.com>
2023-04-15 12:51:37 +1000

    pep8

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -10,12 +10,14 @@ from models.base_model import BaseModel
 from api.v1.views import app_views
 from flask import request, jsonify, abort
 
+
 @app_views.route('/amenities', methods=['GET'], strict_slashes=False)
 def list_amenities():
     amenities_list = []
     for value in storage.all(Amenity).values():
         amenities_list.append(value.to_dict())
     return jsonify(amenities_list)
+
 
 @app_views.route('/amenities/<amenity_id>', methods=['GET'], strict_slashes=False)
 def list_an_amenity(amenity_id):
@@ -26,7 +28,7 @@ def list_an_amenity(amenity_id):
 
 
 @app_views.route('/amenities/<amenity_id>', methods=['DELETE'],
-        strict_slashes=False)
+                 strict_slashes=False)
 def delete_amenity(amenity_id):
     amenity = storage.get(Amenity, amenity_id)
     if amenity is None:
@@ -37,7 +39,7 @@ def delete_amenity(amenity_id):
 
 
 @app_views.route('/amenities', methods=['POST'],
-        strict_slashes=False)
+                 strict_slashes=False)
 def create_amenity():
     json_dict = request.get_json()
     if not json_dict:
@@ -50,7 +52,7 @@ def create_amenity():
 
 
 @app_views.route('/amenities/<amenity_id>', methods=['PUT'],
-        strict_slashes=False)
+                 strict_slashes=False)
 def update_amenity(amenity_id):
     amenity = storage.get(Amenity, amenity_id)
     if amenity is None:
